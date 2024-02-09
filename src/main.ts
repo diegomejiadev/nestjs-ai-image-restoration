12bceb38f6813e8e923acc6acc9c37cf670875b2
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2024-02-08 21:24:00 -0500

    feat: Added validation pipe and global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //* Set a global prefix
+  app.setGlobalPrefix('/api');
+
+  //* Set a global pipe validator
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
